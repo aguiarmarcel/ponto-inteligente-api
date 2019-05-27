a434497f3d3665d55f69d00a7dcac9aa5d73d5e5
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2019-05-27 10:30:15 -0300

    Criando o sevico de empresas

---
+++ src/main/java/com/marcel/pontointeligente/api/services/impl/EmpresaServiceImpl.java
@@ -0,0 +1,32 @@
+package com.marcel.pontointeligente.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcel.pontointeligente.api.domain.Empresa;
+import com.marcel.pontointeligente.api.repositories.EmpresaRepository;
+import com.marcel.pontointeligente.api.services.EmpresaService;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService{
+
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(EmpresaServiceImpl.class);
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	@Override
+	public Optional<Empresa> buscarPorCnpj(String cnpj) {
+		log.info("Buscando uma empresa para o CNPJ {}", cnpj);
+		return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+	}
+	
+	@Override
+	public Empresa persistir(Empresa empresa) {
+		log.info("Persistindo empresa: {}", empresa);
+		return this.empresaRepository.save(empresa);
+	}
+}
