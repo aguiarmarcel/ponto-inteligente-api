d798ef06d1f206c8c9d4ec14f2a95286f6e2a955
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2019-06-11 17:00:27 -0300

    Aumentando a performance com o EhCache

--- src/main/java/com/marcel/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/marcel/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -20,11 +22,13 @@ public class LancamentoServiceImpl implements LancamentoService{
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 	
+	@CachePut("lancamentoPorId")
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo lancamento: {}", lancamento);
 		return this.lancamentoRepository.save(lancamento);
 	}
 	
+	@Cacheable("lancamentoPorId")
 	public Optional<Lancamento> buscarPorId(Long id) {
 		log.info("Buscando um lançamento para o ID {}", id);
 		Optional<Lancamento> obj = this.lancamentoRepository.findById(id);
